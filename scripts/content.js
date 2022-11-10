bf522a4feb1b1b0052f11101bf47995ea2402136
River <foxnezz@gmail.com>
2022-11-11 01:25:38 +0400

    [ADD] add autoclicking share button

--- scripts/content.js
+++ scripts/content.js
@@ -1,25 +1,8 @@
-// const article = document.querySelector("article");
+function main() {
+    const shareButton = $('ytd-segmented-like-dislike-button-renderer').eq(0).next().children().eq(0).children().eq(0)
+    shareButton.click()
+}
 
-// // `document.querySelector` may return null if the selector doesn't match anything.
-// if (article) {
-//     const text = article.textContent;
-//     const wordMatchRegExp = /[^\s]+/g; // Regular expression
-//     const words = text.matchAll(wordMatchRegExp);
-//     // matchAll returns an iterator, convert to array to get word count
-//     const wordCount = [...words].length;
-//     const readingTime = Math.round(wordCount / 200);
-//     const badge = document.createElement("p");
-//     // Use the same styling as the publish information in an article's header
-//     badge.classList.add("color-secondary-text", "type--caption");
-//     badge.textContent = `⏱️ ${readingTime} min read`;
-
-//     // Support for API reference docs
-//     const heading = article.querySelector("h1");
-//     // Support for article docs with date
-//     const date = article.querySelector("time")?.parentNode;
-
-//     (date ?? heading).insertAdjacentElement("afterend", badge);
-// }
-
-// alert("hello im quicksave")
-
+$(window).on('load', function() {
+    setTimeout(main, 1000)
+})
