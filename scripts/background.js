7b39ee12aa6a3d51fd2e719f017783b00e14ef37
River <foxnezz@gmail.com>
2022-11-13 19:08:21 +0400

    [REFACTOR] add youtube

--- scripts/background.js
+++ scripts/background.js
@@ -1,72 +1,14 @@
 import config from '../config/config.js'
+import Youtube from './youtube.js'
 
-const CLIENT_ID = config.web.client_id
-const REDIRECT_URI = config.web.redirect_uris[0]
-const RESPONSE_TYPE = 'token'
-const SCOPE = 'https://www.googleapis.com/auth/youtube'
-const STATE = 'meet' + Math.random().toString(36).substring(2, 15)
-const PROMPT = 'consent'
-
-let userSignedIn = false
-
-function createAuthEndpoint() {
-    let openIdEndpointUrl =
-        `https://accounts.google.com/o/oauth2/v2/auth
-?client_id=${encodeURIComponent(CLIENT_ID)}
-&response_type=${encodeURIComponent(RESPONSE_TYPE)}
-&redirect_uri=${encodeURIComponent(REDIRECT_URI)}
-&scope=${encodeURIComponent(SCOPE)}
-&state=${encodeURIComponent(STATE)}
-&prompt=${encodeURIComponent(PROMPT)}`
-
-    console.log(openIdEndpointUrl)
-    return openIdEndpointUrl
-}
+let youtube = new Youtube(config)
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'login') {
-        if (userSignedIn) {
-            console.log("User is already signed in.")
-            return
-        }
-
-        chrome.identity.launchWebAuthFlow({
-            'url': createAuthEndpoint(),
-            'interactive': true
-        }, (redirectUrl) => {
-            if (chrome.runtime.lastError) {
-                // problem signing in
-                return
-            }
-
-            console.log("redirect url: " + redirectUrl)
-            let idToken = redirectUrl.substring(redirectUrl.indexOf('id_token=') + 9)
-            idToken = idToken.substring(0, idToken.indexOf('&'))
-
-            if (false) {
-                // const user_info = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(idToken.split(".")[1]))
-                // if !((user_info.iss === 'https://accounts.google.com' || user_info.iss === 'accounts.google.com')
-                //     && user_info.aud === CLIENT_ID) {
-                // invalid credentials
-                console.log("Invalid credentials.")
-            }
-
-            console.log("User successfully signed in.")
-            userSignedIn = true
-            chrome.action.setPopup({ popup: '/views/popup-signed-in.html' }, () => {
-                sendResponse('success')
-            })
-        })
-
-        return true
+        youtube.openLoginForm(sendResponse)
     } else if (request.message === 'logout') {
-        userSignedIn = false
-        chrome.action.setPopup({ popup: '/views/popup.html' }, () => {
-            sendResponse('success')
-        })
-
-        return true
+        youtube.logout(sendResponse)
     } else if (request.message === 'isUserSignedIn') {
-        sendResponse(userSignedIn)
+        sendResponse(youtube.signedIn)
     }
 })
---
+++ scripts/youtube.js
@@ -0,0 +1,73 @@
+class Youtube {
+    constructor(config) {
+        this.CLIENT_ID = config.web.client_id
+        this.REDIRECT_URI = config.web.redirect_uris[0]
+        this.RESPONSE_TYPE = 'token'
+        this.SCOPE = 'https://www.googleapis.com/auth/youtube'
+        this.STATE = 'meet' + Math.random().toString(36).substring(2, 15)
+        this.PROMPT = 'consent'
+
+        this.signedIn = false
+    }
+
+    createAuthEndpoint() {
+        let openIdEndpointUrl =
+            `https://accounts.google.com/o/oauth2/v2/auth
+?client_id=${encodeURIComponent(this.CLIENT_ID)}
+&response_type=${encodeURIComponent(this.RESPONSE_TYPE)}
+&redirect_uri=${encodeURIComponent(this.REDIRECT_URI)}
+&scope=${encodeURIComponent(this.SCOPE)}
+&state=${encodeURIComponent(this.STATE)}
+&prompt=${encodeURIComponent(this.PROMPT)}`
+
+        console.log(openIdEndpointUrl)
+        return openIdEndpointUrl
+    }
+
+    openLoginForm(sendResponse) {
+        if (this.signedIn) {
+            console.log("User is already signed in.")
+            sendResponse('fail')
+            return
+        }
+
+        chrome.identity.launchWebAuthFlow({
+            'url': this.createAuthEndpoint(),
+            'interactive': true
+        }, (redirectUrl) => {
+            if (chrome.runtime.lastError) {
+                sendResponse('fail')
+                return
+            }
+
+            console.log("redirect url: " + redirectUrl)
+            let idToken = redirectUrl.substring(redirectUrl.indexOf('id_token=') + 9)
+            idToken = idToken.substring(0, idToken.indexOf('&'))
+
+            // if (false) {
+            //     // const user_info = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(idToken.split(".")[1]))
+            //     // if !((user_info.iss === 'https://accounts.google.com' || user_info.iss === 'accounts.google.com')
+            //     //     && user_info.aud === CLIENT_ID) {
+            //     // invalid credentials
+            //     console.log("Invalid credentials.")
+            // }
+
+            console.log("User successfully signed in.")
+            this.signedIn = true
+
+            chrome.action.setPopup({ popup: '/views/popup-signed-in.html' }, () => {
+                sendResponse('success')
+            })
+        })
+    }
+
+    logout(sendResponse) {
+        this.signedIn = false
+
+        chrome.action.setPopup({ popup: '/views/popup.html' }, () => {
+            sendResponse('success')
+        })
+    }
+}
+
+export default Youtube
