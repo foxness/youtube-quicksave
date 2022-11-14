10bbc776260685caccc7a33bcada15431c0a671c
River <foxnezz@gmail.com>
2022-11-14 09:46:11 +0400

    [REFACTOR] refactor auth endpoint creation

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -196,27 +196,29 @@ class Youtube {
         } else {
             popup = '/views/popup.html'
         }
-        
+
         await chrome.action.setPopup({ popup: popup })
     }
 
     createAuthEndpoint() {
-        this.RESPONSE_TYPE = 'token'
+        let endpoint = 'https://accounts.google.com/o/oauth2/v2/auth'
+
         this.SCOPE = 'https://www.googleapis.com/auth/youtube'
         this.STATE = 'meet' + Math.random().toString(36).substring(2, 15)
-        this.PROMPT = 'consent'
 
-        let openIdEndpointUrl =
-            `https://accounts.google.com/o/oauth2/v2/auth
-?client_id=${encodeURIComponent(this.CLIENT_ID)}
-&response_type=${encodeURIComponent(this.RESPONSE_TYPE)}
-&redirect_uri=${encodeURIComponent(this.REDIRECT_URI)}
-&scope=${encodeURIComponent(this.SCOPE)}
-&state=${encodeURIComponent(this.STATE)}
-&prompt=${encodeURIComponent(this.PROMPT)}`
+        let data = {
+            client_id: this.CLIENT_ID,
+            response_type: 'token',
+            redirect_uri: this.REDIRECT_URI,
+            scope: this.SCOPE,
+            state: this.STATE,
+            prompt: 'consent'
+        }
 
-        console.log(openIdEndpointUrl)
-        return openIdEndpointUrl
+        let url = new URL(endpoint)
+        url.search = new URLSearchParams(data)
+
+        return url.toString()
     }
 }
 
